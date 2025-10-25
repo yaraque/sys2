8f4b45c714539492dff287340fe6e9fbec2ea171
yaraque <yusmelymac18@gmail.com>
2025-10-24 23:20:03 -0400

    estructura basica

---
+++ admin/roles/index.php
@@ -0,0 +1,41 @@
+<?php
+    include_once("../../app/variables.php");
+    include("../layout/header.php");
+?>
+    <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Starter Page</h1>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="container">
+          <div class="col-lg-6">
+            dsfsd
+          </div>
+          <!-- /.col-md-6 -->
+          <div class="col-lg-6">
+            
+          </div>
+          <!-- /.col-md-6 -->
+        </div>
+        <!-- /.container -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  
+  
+<?php
+    include("../layout/footer.php");
+?>
