11010feeccccc7672ed05e39c6f5a4c979c80de3
yaraque <yusmelymac18@gmail.com>
2025-10-25 09:15:49 -0400

    actualizacion de tabla basica

--- admin/roles/index.php
+++ admin/roles/index.php
@@ -1,30 +1,34 @@
 <?php
     include_once("../../app/variables.php");
     include("../layout/header.php");
+    include_once("../../app/controllers/roles/lista_roles.php");
 ?>
     <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <br>
     <div class="content">
-      <div class="container-fluid">
-        <div class="row-fluid">
+      <div class="container">
+        <div class="row">
           <h1><b>Lista de Roles</b></h1>
-          <hr>
-          <table>
-            <head>
+        </div>
+        <div class="row">
+          <table border="1">
+            <thead>
               <tr>
                 <th>Id</th>
-                <th>Descrpción de Rol</th>
+                 <th>Descrpción de Rol</th>
                 <th>Acciones</th>
               </tr>
-            </head>
-            <body>
-              <tr></tr>
-              <tr></tr>
-              <tr></tr>
-            </body>
+            </thead>
+            <tbody>
+              <tr>  
+              <td>1</td>
+              <td>administrador</td>
+              <td>registrar ver editar</td>
+              </tr>
+            </tbody>
           </table>
-        </div>
+          </div>
       </div>
     </div>
     
