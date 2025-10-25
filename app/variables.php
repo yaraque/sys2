33f4aab9b3312627deade74d37b6c786aa9e770f
yaraque <yusmelymac18@gmail.com>
2025-10-24 20:17:56 -0400

    archivo dedicado a variables globales

---
+++ app/variables.php
@@ -0,0 +1,5 @@
+<?php
+    define('app_name','SISTEMA DE INSCRIPCIÓN'); //Nombre del sistema
+    define('app_inst', 'NUEVO HORIZONTE'); //Nombre de la institución
+    define('app_url', 'http://localhost/sys2/'); //URL
+?>
